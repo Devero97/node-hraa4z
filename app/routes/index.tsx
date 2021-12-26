d900121d0505c770ae395a0f5d2efa80acea8b2a
Devero97 <kasperchuk72@yandex.ru>
2021-12-26 19:02:17 +0300

    Initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+import Editor from '~/utils/editor';
+export default function Index() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <h1>Editor js</h1>
+      <Editor />
+    </div>
+  );
+}
